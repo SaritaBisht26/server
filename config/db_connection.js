09a2c87f3c26f683c04b2e27a7313b37b63edfc6
Pavitar Singh <pavitar.1127@zenmonk.tech>
2024-04-10 00:28:52 +0530

    update

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,13 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
+
 const connectToDB = () => {
+  // Set strictQuery option to false
+  mongoose.set('strictQuery', false);
+
   mongoose
     .connect(process.env.MONGO_URL)
     .then(() => {
       console.log("connection established successfully!");
     })
-    .catch((err) => console.log("some error occured",err.message));
+    .catch((err) => console.log("some error occurred", err.message));
 };
 
 export default connectToDB;
